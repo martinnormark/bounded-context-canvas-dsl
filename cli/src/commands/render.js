2ed28c2b09b343cf58167297158dcab4bc7353cd
Martin Høst Normark <martin.hoest.normark@lego.com>
2024-04-25 22:16:05 +0200

    Update render command to take screenshot of the server page

--- cli/src/commands/render.js
+++ cli/src/commands/render.js
@@ -5,16 +5,12 @@ module.exports = async (bccFilePath, outputPath) => {
 		headless: true,
 	});
 	const page = await browser.newPage();
-	await page.setContent(`<!DOCTYPE html>
-  <html>
-  <body style="display: flex; justify-content: center; align-items: center;">
-    <svg width="100" height="100">
-      <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
-    </svg>
-  </body>
-  </html>`);
-	const svg = await page.$('svg');
-	const svgContent = await page.evaluate((element) => element.outerHTML, svg);
-	console.log(svgContent);
+	await page.setViewport({ width: 1920, height: 1200 });
+	await page.goto(`http://localhost:3000/view.html`);
+	await page.waitForSelector('body');
+
+	// take screenshot of the page
+	await page.screenshot({ path: outputPath, fullPage: true });
+
 	await browser.close();
 };
