e9494b340f5786195d049404db0dde061f8500e5
Martin Høst Normark <martin.hoest.normark@lego.com>
2024-04-25 22:15:42 +0200

    Bind data to template via Handlebars

--- cli/src/commands/serve.js
+++ cli/src/commands/serve.js
@@ -1,12 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 const bs = require('browser-sync').create();
+const handlebars = require('handlebars');
+
+function loadTemplate() {
+	return fs.readFileSync(path.resolve(__dirname, '../template.html'), 'utf8');
+}
+
+function loadCanvasData(bccFilePath) {
+	return fs.readFileSync(bccFilePath, 'utf8');
+}
 
 module.exports = async (bccFilePath, port = 3000) => {
-	const template = fs.readFileSync(path.resolve(__dirname, '../template.html'), 'utf8');
-	const data = JSON.parse(fs.readFileSync(bccFilePath, 'utf8'));
-	const rendered = template.replace('{{data}}', JSON.stringify(data));
-
 	bs.init({
 		files: [path.resolve(__dirname, '../template.html'), bccFilePath],
 		port,
@@ -14,5 +19,26 @@ module.exports = async (bccFilePath, port = 3000) => {
 			baseDir: path.resolve(__dirname, '../'),
 			index: 'template.html',
 		},
+		startPath: '/view.html',
+		middleware: [
+			{
+				route: '/data.json',
+				handle: function (req, res, next) {
+					res.setHeader('Content-Type', 'application/json');
+					res.end(loadCanvasData(bccFilePath));
+				},
+			},
+			{
+				route: '/view.html',
+				handle: function (req, res, next) {
+					const html = loadTemplate();
+					const json = loadCanvasData(bccFilePath);
+					const data = JSON.parse(json);
+					const template = handlebars.compile(html);
+					res.setHeader('Content-Type', 'text/html');
+					res.end(template(data));
+				},
+			},
+		],
 	});
 };
