d4b3cf5788e6de5699e418764c8319801d30c3c6
Martin Høst Normark <martin.hoest.normark@lego.com>
2024-05-01 08:50:51 +0200

    Add command to create a Bounded Context Canvas file

---
+++ cli/src/commands/new.js
@@ -0,0 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import example from '../../../examples/order-mgt.json';
+
+const newCanvas = async (name, workingDirectory) => {
+	console.log(`Creating new canvas: ${name}`);
+
+	const dirPath = workingDirectory || process.cwd();
+	let canvasDir = dirPath;
+
+	if (!path.isAbsolute(dirPath)) {
+		console.log(`The provided directory path "${dirPath}" is a relative path.`);
+		canvasDir = path.resolve(process.cwd(), dirPath);
+	}
+
+	// sanitize name for file-name
+	const fileName = name.toLowerCase().replace(/\s+/g, '-');
+	const filePath = path.resolve(canvasDir, `${fileName}.json`);
+
+	fs.writeFileSync(filePath, JSON.stringify(example, null, 2) + '\n', 'utf8');
+};
+
+export default newCanvas;
--- cli/src/index.js
+++ cli/src/index.js
@@ -2,11 +2,13 @@
 
 import { Command } from 'commander';
 const program = new Command();
+import newCanvas from './commands/new.js';
 import validate from './commands/validate.js';
 import render from './commands/render.js';
 import serve from './commands/serve.js';
 
 program.name('bc-canvas').version('0.1.0');
+program.command('new <name> [workingDirectory]').description('Create a Bounded Context Canvas file').action(newCanvas);
 program.command('validate <bccFilePath> [schemaFilePath]').description('Validate a Bounded Context Canvas file').action(validate);
 program.command('render <bccFilePath> <outputPath>').description('Render a Bounded Context Canvas file').action(render);
 program.command('serve <bccFilePath> [background] [port]').description('Serve HTML version of a Bounded Context Canvas file').action(serve);
